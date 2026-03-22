cecd026cdcb06fc5ddad7120d57aacfd7242d838
Dinesh Kumar <dinesh051833@gmail.com>
2026-03-22 14:20:03 +0530

    fix startup

--- main.py
+++ main.py
@@ -3,14 +3,9 @@ from api.routes import router
 from starlette.middleware.sessions import SessionMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
-from rag.rag_pipeline import load_vector_db
 
 app = FastAPI()
 
-
-@app.on_event("startup")
-def startup_event():
-    load_vector_db()
 
 
 app.add_middleware(
